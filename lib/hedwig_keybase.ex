0fcf8912d18bc193d86738c00639373afefbd110
Jeff Jewiss <jeff@jeffjewiss.com>
2018-05-15 13:43:12 +0100

    mix new

---
+++ lib/hedwig_keybase.ex
@@ -0,0 +1,17 @@
+defmodule HedwigKeybase do
+  @moduledoc """
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HedwigKeybase.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hedwig_keybase_test.exs
@@ -0,0 +1,8 @@
+defmodule HedwigKeybaseTest do
+  use ExUnit.Case
+  doctest HedwigKeybase
+
+  test "greets the world" do
+    assert HedwigKeybase.hello() == :world
+  end
+end
